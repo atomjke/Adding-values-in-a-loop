f373e622781c7060cc3d4e67f3d82684805ba6aa
Evgeny Turmy <jenqa29@mail.ru>
2021-11-07 09:16:57 +0200

    Project added

---
+++ src/App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        int inputNumber, sum = 0;
+
+        System.out.println("I will add up the numbers you give me.");
+        System.out.print("Number: ");
+
+        inputNumber = input.nextInt();
+
+        while (inputNumber != 0) {
+            sum += inputNumber;
+
+            System.out.println("The total so far is " + sum + ".");
+            System.out.print("Number: ");
+
+            inputNumber = input.nextInt();
+        }
+
+        System.out.println("The total is " + sum + ".");
+        
+        input.close();
+    }
+}
